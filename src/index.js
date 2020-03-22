66731c470c0fb6548a33d1aca96604db35fc2869
Maria Kuzmicheva <kuzmichevamariia@gmail.com>
2020-03-22 15:53:37 +0100

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    const allNumbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+        "twenty"
+    ];
+
+    const dozens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety"
+    ];
+
+    for (let i = 21; i < 1000; i++) {
+        if (i < 100)
+            allNumbers.push(
+                dozens[Math.floor(i / 10)] +
+                    (i % 10 == 0 ? "" : " " + allNumbers[i % 10])
+            );
+        else
+            allNumbers.push(
+                allNumbers[Math.floor(i / 100)] +
+                    " hundred" +
+                    (i % 100 == 0 ? "" : " " + allNumbers[i % 100])
+            );
+    }
+    return allNumbers[number];
+};
